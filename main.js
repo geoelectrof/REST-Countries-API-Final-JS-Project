4cb3562d0ed5a9fa8d27c78ef9d3523fd14cbc2d
geoelectrof <dimitris.geo@gmail.com>
2022-05-19 23:03:38 +0300

    Filter by continet functionality

--- main.js
+++ main.js
@@ -1,31 +1,41 @@
 // GLOBAL VARIABLES
 let countries; //will contain the fetched data
+const continentsList = document.getElementById("continents");
 const countriesList = document.getElementById("countries");
 
+function fetchCountries(continent) {
+    // console.log(1);
+    fetch(`https://restcountries.com/v2/${continent}`)
+        .then(function (res) {
+            // console.log(2);
+            // console.log(res);
+            return res.json();
+        })
+        .then(function (data) {
+            // console.log(data);
+            initialize(data);
+            // console.log(3);
+        })
+        .catch(function (err) {
+            console.log("Error: " + err);
+        })
+    // console.log(4);
+}
+fetchCountries("all");
+
+
 //Event Listener
-countriesList.addEventListener("change", function (event) {
+continentsList.addEventListener("change", function (event) {
     // console.log(event);
-    // console.log(event.target.value);
-    displayCountryInfo(event.target.value);
+    console.log(event.target.value);
+    let selectedContinent = event.target.value;
+    if (selectedContinent === "all") {
+        fetchCountries("all");
+    } else {
+        fetchCountries("region/" + selectedContinent);
+    }
+    // displayCountryInfo(event.target.value);
 })
-
-// console.log(1);
-fetch("https://restcountries.com/v2/all")
-    .then(function (res) {
-        // console.log(2);
-        // console.log(res);
-        return res.json();
-    })
-    .then(function (data) {
-        // console.log(data);
-        initialize(data);
-        // console.log(3);
-    })
-    .catch(function (err) {
-        console.log("Error: " + err);
-    })
-// console.log(4);
-
 
 function initialize(countriesData) {
     // console.log(countriesData);
@@ -35,12 +45,14 @@ function initialize(countriesData) {
     for (let i = 0; i < countries.length; i++) {
         // options += `<option value="${countries[i].alpha3Code}">${countries[i].name}</option>`;
         countriesDiv +=
-            `<p><strong>Country: <span id="country-name">${countries[i].name}</span></strong></p>
+            `<div>
+            <p><strong>Country: <span id="country-name">${countries[i].name}</span></strong></p>
             <img src="${countries[i].flag}" alt="">
             <p>Capital: <span id="capital">${countries[i].capital}</span></p>
             <p>Population: <span id="population">${countries[i].population.toLocaleString("en-US")}</span></p>
             <p>Region: <span id="region">${countries[i].region}</span></p>
-        <p>Subregion: <span id="subregion">${countries[i].subregion}</span></p>`;
+            <p>Subregion: <span id="subregion">${countries[i].subregion}</span></p>
+            </div>`;
         // `<option value="${countries[i].alpha3Code}">${countries[i].name} (+${countries[i].callingCodes[0]})</option>`;
     }
     countriesList.innerHTML = countriesDiv;
