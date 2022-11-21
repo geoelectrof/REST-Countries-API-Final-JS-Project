76ea3d8e78f6417304f596a2218722e4490a5f8f
geoelectrof <dimitris.geo@gmail.com>
2022-11-21 17:42:13 +0200

    Use of .forEach in initialize funtion

--- main.js
+++ main.js
@@ -54,19 +54,32 @@ function initialize(countriesData) {
     // console.log(countriesData[0].name);
     countries = countriesData;
     let countriesDiv = "";
-    for (let i = 0; i < countries.length; i++) {
-        // options += `<option value="${countries[i].alpha3Code}">${countries[i].name}</option>`;
+    countries.forEach(country => {
         countriesDiv +=
             `<div class="country-single-card">
-            <p><strong>Country: <span id="country-name">${countries[i].name}</span></strong></p>
-            <img src="${countries[i].flag}" alt="">
-            <p>Capital: <span id="capital">${countries[i].capital}</span></p>
-            <p>Population: <span id="population">${countries[i].population.toLocaleString("en-US")}</span></p>
-            <p>Region: <span id="region">${countries[i].region}</span></p>
-            <p>Subregion: <span id="subregion">${countries[i].subregion}</span></p>
-            </div>`;
-        // `<option value="${countries[i].alpha3Code}">${countries[i].name} (+${countries[i].callingCodes[0]})</option>`;
-    }
+            <p><strong>Country: <span id="country-name">${country.name}</span></strong></p>
+            <img src="${country.flag}" alt="" width="200px">
+            <p>Capital: <span id="capital">${country.capital}</span></p>
+            <p>Population: <span id="population">${country.population.toLocaleString("en-US")}</span></p>
+            <p>Region: <span id="region">${country.region}</span></p>
+            <p>Subregion: <span id="subregion">${country.subregion}</span></p>
+            </div>
+            <br />
+            <br />`;
+    })
+    // for (let i = 0; i < countries.length; i++) {
+    //     // options += `<option value="${countries[i].alpha3Code}">${countries[i].name}</option>`;
+    //     countriesDiv +=
+    //         `<div class="country-single-card">
+    //         <p><strong>Country: <span id="country-name">${countries[i].name}</span></strong></p>
+    //         <img src="${countries[i].flag}" alt="">
+    //         <p>Capital: <span id="capital">${countries[i].capital}</span></p>
+    //         <p>Population: <span id="population">${countries[i].population.toLocaleString("en-US")}</span></p>
+    //         <p>Region: <span id="region">${countries[i].region}</span></p>
+    //         <p>Subregion: <span id="subregion">${countries[i].subregion}</span></p>
+    //         </div>`;
+    //     // `<option value="${countries[i].alpha3Code}">${countries[i].name} (+${countries[i].callingCodes[0]})</option>`;
+    // }
     countriesList.innerHTML = countriesDiv;
     // countriesList.selectedIndex = Math.floor(Math.random() * countriesList.length);
 
@@ -82,10 +95,12 @@ countriesCards.forEach((card) => {
 //1. .fetch remove initialize function
 //2. At initialize function use .forEach
 //3. When typing in input search only in the selected continent
-//4. Style with CSS and BEM (practise)
+//4. Style with CSS and BEM (practice)
 //5. On country click show country's details on modal window
 //5.1 Show countries with shared borders & display them on modal window
 //5.2 Return back button
+//6. Apply light/dark mode
+//7. Write Readme documentation and comments
 
 
 
